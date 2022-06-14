bc967f3298a5dc8d3b509ff46b0f85e2f12581e9
Raphael de Melo <raphaeldemelosoares@hotmail.com>
2022-06-14 12:01:26 -0300

    feat: implementando sectiontitle , título e botão

--- src/page/Home/index.jsx
+++ src/page/Home/index.jsx
@@ -1,9 +1,30 @@
-import { Container, Header } from './styles'
+import { FaFilter } from 'react-icons/fa'
+import {
+  Container,
+  Header,
+  SectionTitle,
+  Title,
+  Subtitle,
+  Button,
+  Text
+} from './styles'
 
 export default function Home () {
   return (
     <Container>
       <Header />
+
+      <SectionTitle>
+        <div>
+          <Title>Dashboard</Title>
+          <Subtitle>Desafio Técnico Frontend</Subtitle>
+        </div>
+
+        <Button>
+          <FaFilter size={13} color='var(--color-white)' />
+          <Text>Filtrar</Text>
+        </Button>
+      </SectionTitle>
     </Container>
   )
 }
