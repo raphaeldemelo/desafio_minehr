9cf117bd86f70bf5e5281de98e5170c87c6a27a9
Raphael de Melo <raphaeldemelosoares@hotmail.com>
2022-06-14 12:02:35 -0300

    style: estilizando sectiontitle

--- src/page/Home/styles.jsx
+++ src/page/Home/styles.jsx
@@ -10,3 +10,32 @@ export const Header = styled.header`
   display: flex;
   height: 91px;
 `
+export const SectionTitle = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 28px;
+`
+export const Title = styled.h1`
+  color: var(--color-white);
+  font-size: 28px;
+`
+export const Subtitle = styled.strong`
+  color: var(--color-gray);
+  font-size: 14px;
+  font-weight: 500;
+`
+export const Button = styled.button`
+  background-color: var(--color-green);
+  width: 100px;
+  height: 40px;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`
+export const Text = styled.strong`
+  color: var(--color-gray);
+`
